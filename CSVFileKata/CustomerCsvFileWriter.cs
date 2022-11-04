cf1c1b0f7069126b36cce3f2df57b37e2dddc13d
Silvio Duka <silvio.duka@gmail.com>
2022-11-04 08:00:53 +0100

    Moved class to proper project

---
+++ CSVFileKata/CustomerCsvFileWriter.cs
@@ -0,0 +1,16 @@
+﻿namespace CSVFileKata;
+
+public class CustomerCsvFileWriter
+{
+    private readonly IFileSystem _fileSystem;
+
+    public CustomerCsvFileWriter(IFileSystem fileSystem)
+    {
+        _fileSystem = fileSystem;
+    }
+
+    public void Write(string fileName, Customer customer)
+    {
+        _fileSystem.WriteLine("", "Brandon Page,1234555678");
+    }
+}
--- CSVFileKataTests/CsvFileKataTests.cs
+++ CSVFileKataTests/CsvFileKataTests.cs
@@ -26,19 +26,4 @@ namespace CSVFileKataTests
             fileSystem.Received(1).WriteLine("", "Brandon Page,1234555678");
         }
     }
-
-    public class CustomerCsvFileWriter
-    {
-        private readonly IFileSystem _fileSystem;
-
-        public CustomerCsvFileWriter(IFileSystem fileSystem)
-        {
-            _fileSystem = fileSystem;
-        }
-
-        public void Write(string fileName, Customer customer)
-        {
-            _fileSystem.WriteLine("", "Brandon Page,1234555678");
-        }
-    }
 }
